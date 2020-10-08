0f5b7c744b570c48b9ad53479e51299cf8ab28ee
Rahul <rahul@di.ku.dk>
2020-10-08 21:34:18 +0200

    uniform eval mode

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 1. Create conda env: `conda create -n pos-bert python=3.7`
 2. Install dependencies: `pip install -r requirements.txt`
 3. Download UD: Use [this link](https://lindat.mff.cuni.cz/repository/xmlui/handle/11234/1-3226) to download all treebanks, unzip and place them inside `data` folder
-4. TODO: Make a script to create the 'x' folder inside `data`
+4. TODO: Make a script to create the `x` folder inside `data`
 5. Create new configs in the `configs` folder and use them to start training
 
 Trainer usage
--- tester.py
+++ tester.py
@@ -22,21 +22,21 @@ logger = logging.getLogger(__name__)
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
-def full_evaluate(test_set, label_map, bert_model, clf_head, config):
+def zero_shot_evaluate(test_set, label_map, bert_model, clf_head, config):
     loader = DataLoader(test_set, batch_size=config.batch_size, collate_fn=utils.pos_collate_fn)
     loss, metrics = utils.compute_loss_metrics(loader, bert_model, clf_head, label_map)
     metrics.update({"loss": loss.item()})
     return metrics
 
 
-def meta_evaluate(dataset, label_map, bert_model, clf_head, config):
-    meta_dataset = l2l.data.MetaDataset(dataset)
-    test_task = l2l.data.TaskDataset(
+def evaluate(train_set, test_set, label_map, bert_model, clf_head, shots, config):
+    meta_dataset = l2l.data.MetaDataset(train_set)
+    train_task = l2l.data.TaskDataset(
         meta_dataset,
         num_tasks=config.num_tasks,
         task_transforms=[
             # here n=1 because the labels are language ids
-            l2l.data.transforms.FusedNWaysKShots(meta_dataset, n=1, k=config.shots),
+            l2l.data.transforms.FusedNWaysKShots(meta_dataset, n=1, k=shots),
             l2l.data.transforms.LoadData(meta_dataset),
         ],
     )
@@ -45,16 +45,15 @@ def meta_evaluate(dataset, label_map, bert_model, clf_head, config):
     inner_loop_steps = config.inner_loop_steps
     inner_lr = config.inner_lr
 
+    query_loader = DataLoader(test_set, batch_size=config.batch_size, collate_fn=utils.pos_collate_fn)
     meta_model = l2l.algorithms.MAML(clf_head, lr=inner_lr, first_order=config.is_fomaml)
 
-    task_support_error, task_query_error = 0.0, []
+    task_support_error = 0.0
     tqdm_bar = tqdm(range(num_episodes))
     all_metrics = {"p": [], "r": [], "f": []}
     for _ in tqdm_bar:
         learner = meta_model.clone()
-        support_task, _ = test_task.sample()
-        query_task, _ = test_task.sample()
-
+        support_task, _ = train_task.sample()
         for _ in range(inner_loop_steps):
             support_loader = DataLoader(
                 data_utils.InnerPOSDataset(support_task), batch_size=task_bs, shuffle=True, num_workers=0
@@ -64,11 +63,7 @@ def meta_evaluate(dataset, label_map, bert_model, clf_head, config):
             l2l.algorithms.maml_update(learner, inner_lr, grads)
             task_support_error += support_error
 
-        query_loader = DataLoader(
-            data_utils.InnerPOSDataset(query_task), batch_size=task_bs, shuffle=True, num_workers=0
-        )
         query_error, metrics = utils.compute_loss_metrics(query_loader, bert_model, learner, label_map)
-        task_query_error.append(query_error)
         tqdm_bar.set_description("Query Loss: {:.3f}".format(query_error.item()))
 
         all_metrics["p"].append(metrics["precision"])
@@ -86,48 +81,44 @@ def meta_evaluate(dataset, label_map, bert_model, clf_head, config):
 
 def init_args():
     parser = argparse.ArgumentParser(description="Test POS tagging on various UD datasets")
-    parser.add_argument("--test_path", dest="test_path", type=str, help="Dataset to test on", required=True)
+    parser.add_argument("--test_lang", dest="test_lang", type=str, help="Language to test on", required=True)
     parser.add_argument("--model_path", dest="model_path", type=str, help="Path of the model to load", required=True)
     parser.add_argument(
-        "-e",
-        "--eval_type",
-        help="Type of evaluation to perform",
-        choices=["meta", "full", "both"],
-        default="both",
+        "--shots", dest="shots", type=int, help="Number of examples to use for finetuning", required=True
     )
     return parser.parse_args()
 
 
 def main():
     args = init_args()
-    config = model_utils.Config(os.path.join(args.model_path, "config.json"))
+    config_path = os.path.join(args.model_path, "config.json")
+    load_path = os.path.join(args.model_path, "best_model.th")
+    train_path = os.path.join("data/x", "{}.train".format(args.test_lang))
+    test_path = os.path.join("data/x", "{}.test".format(args.test_lang))
+    logging.info("Loading config from path: {}".format(config_path))
+    logging.info("Loading model from path: {}".format(load_path))
+    logging.info("Loading train data from path: {}".format(train_path))
+    logging.info("Loading test data from path: {}".format(test_path))
 
-    # for reproducibility
+    config = model_utils.Config(config_path)
     torch.manual_seed(config.seed)
 
-    test_set = data_utils.POS(args.test_path, config.max_seq_length, config.model_type)
+    train_set = data_utils.POS(train_path, config.max_seq_length, config.model_type)
+    test_set = data_utils.POS(test_path, config.max_seq_length, config.model_type)
     label_map = {idx: l for idx, l in enumerate(data_utils.get_pos_labels())}
 
-    if args.eval_type == "both":
-        eval_types = ["full", "meta"]
+    bert_model = model_utils.BERT(config)
+    bert_model = bert_model.eval().to(DEVICE)
+    clf_head = model_utils.SeqClfHead(len(label_map), config.hidden_dropout_prob, bert_model.get_hidden_size())
+    clf_head.load_state_dict(torch.load(load_path))
+    clf_head = clf_head.to(DEVICE)
+
+    shots = args.shots
+    if shots == 0:
+        summary_metrics = zero_shot_evaluate(test_set, label_map, bert_model, clf_head, config)
     else:
-        eval_types = [args.eval_type]
-    for eval_type in eval_types:
-        bert_model = model_utils.BERT(config)
-        bert_model = bert_model.eval().to(DEVICE)
-        clf_head = model_utils.SeqClfHead(len(label_map), config.hidden_dropout_prob, bert_model.get_hidden_size())
-
-        load_path = os.path.join(args.model_path, "best_model.th")
-        logging.info("Loading model from path: {}".format(load_path))
-        clf_head.load_state_dict(torch.load(load_path))
-        clf_head = clf_head.to(DEVICE)
-
-        logging.info("Running {} evaluation".format(eval_type))
-        if eval_type == "full":
-            summary_metrics = full_evaluate(test_set, label_map, bert_model, clf_head, config)
-        elif eval_type == "meta":
-            summary_metrics = meta_evaluate(test_set, label_map, bert_model, clf_head, config)
-        logger.info(json.dumps(summary_metrics, indent=2))
+        summary_metrics = evaluate(train_set, test_set, label_map, bert_model, clf_head, shots, config)
+    logger.info("{} shot results:\n{}".format(shots, json.dumps(summary_metrics, indent=2)))
 
 
 if __name__ == "__main__":
